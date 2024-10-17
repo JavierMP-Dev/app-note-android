b97e743bfdf07db7ad94c449df995f1a1be4212c
Javier <montoya.javier.15@hotmail.com>
2024-10-16 23:03:46 -0600

    camios es NotesViewModel

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/presentation/notes/NotesViewModel.kt
@@ -1,6 +1,9 @@
 package com.plcoding.cleanarchitecturenoteapp.presentation.notes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.plcoding.cleanarchitecturenoteapp.domain.use_case.DeleteNote
 import com.plcoding.cleanarchitecturenoteapp.domain.use_case.NoteCaseUses
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +12,27 @@ import javax.inject.Inject
 class NotesViewModel @Inject constructor(
     private val noteCaseUses: NoteCaseUses
 ) : ViewModel(){
+
+    private val  _state = mutableStateOf(NotesState())
+    val state:State<NotesState> = _state
+    fun onEvent (event: NotesEvent){
+        when (event) {
+            is NotesEvent.Order -> {
+
+            }
+            is NotesEvent.DeleteNote->{
+
+
+             }
+            is NotesEvent.RestoreNote ->{
+
+            }
+            is NotesEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+        }
+    }
 
 }
