ade055405a5e2483c560b0a726f65cdaf4208736
Javier <montoya.javier.15@hotmail.com>
2024-10-16 22:13:51 -0600

    clase NotesEvent

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/presentation/notes/NotesEvent.kt
@@ -1,4 +1,11 @@
 package com.plcoding.cleanarchitecturenoteapp.presentation.notes
 
-class NotesEvent {
+import com.plcoding.cleanarchitecturenoteapp.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.domain.util.NoteOrder
+
+sealed class NotesEvent {
+    data class Order (val noteOrder: NoteOrder) : NotesEvent()
+    data class DeleteNote(val note: Note): NotesEvent()
+    object RestoreNote:  NotesEvent()
+    object ToggleOrderSection: NotesEvent()
 }
